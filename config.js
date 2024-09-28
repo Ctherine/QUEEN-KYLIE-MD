4e5d9c234c9f56a2b2b1ee128be9f8781b184d21
Ctherine <katsandetadiwa621@gmail.com>
2024-09-28 16:43:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ZikG2Cxb#lbqZHA5f1DEGpOg0ZUQodwfMjWr0ImaiXfyuNUUb9sk",
+SESSION_ID: process.env.SESSION_ID || "eQ0kQTqY#-F4PLgJJ8tP9JLJZkL4_W7XdOynIu_gc3jVY2cBfwWQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tCkmbMx/IMG-20240913-WA0041.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389",
-SUDO_NB: process.env.SUDO_NB || "94718913389",
+SUDO_NB: process.env.SUDO_NB || "447789751820",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true" 
